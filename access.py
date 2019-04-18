c7b562b018590107b7fc896eb57a0d412d1baee6
Isaiah Chen <isaiahchen28@gmail.com>
2019-04-18 18:56:31 -0400

    Update access.py

--- access.py
+++ access.py
@@ -4,21 +4,23 @@ This Python script is used to access Twitter.
 import config
 import tweepy
 
-def main():
-	# Retrieve API keys and tokens for the app
-	consumer_key = config.consumer_key
-	consumer_secret = config.consumer_secret
-	access_token = config.access_token
-	access_secret = config.access_secret
-	# Use OAuth interface to authorize the app to access Twitter
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_token, access_secret)
-	api = tweepy.API(auth)
 
-	for status in tweepy.Cursor(api.home_timeline).items(10):
-	    # Process a single status
-	    print(status.text)
-	    print("\n")
+def main():
+    # Retrieve API keys and tokens for the app
+    consumer_key = config.consumer_key
+    consumer_secret = config.consumer_secret
+    access_token = config.access_token
+    access_secret = config.access_secret
+    # Use OAuth interface to authorize the app to access Twitter
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    api = tweepy.API(auth)
+
+    for status in tweepy.Cursor(api.home_timeline).items(10):
+        # Process a single status
+        print(status.text)
+        print("\n")
+
 
 if __name__ == '__main__':
-	main()
+    main()
