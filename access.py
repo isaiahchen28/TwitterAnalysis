ef959bc9f43dbc92d877aaabbe15c9428228bd65
Isaiah Chen <isaiahchen28@gmail.com>
2019-04-18 17:25:21 -0400

    Update access.py

--- access.py
+++ access.py
@@ -1,6 +1,24 @@
-from twython import Twython
+"""
+This Python script is used to access Twitter.
+"""
 import config
+import tweepy
 
-# Create a Twython object by passing the necessary secret passwords
-twitter = Twython(config.api_key, config.api_secret, config.access_token, config.token_secret)
-print(twitter)
+def main():
+	# Retrieve API keys and tokens for the app
+	consumer_key = config.consumer_key
+	consumer_secret = config.consumer_secret
+	access_token = config.access_token
+	access_secret = config.access_secret
+	# Use OAuth interface to authorize the app to access Twitter
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_secret)
+	api = tweepy.API(auth)
+
+	for status in tweepy.Cursor(api.home_timeline).items(10):
+	    # Process a single status
+	    print(status.text)
+	    print("\n")
+
+if __name__ == '__main__':
+	main()
