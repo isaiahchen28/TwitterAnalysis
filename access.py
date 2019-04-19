7db13fc2ea1ca0e01967b63acb7a99af8fdb6077
Isaiah Chen <isaiahchen28@gmail.com>
2019-04-19 14:58:26 -0400

    Update local repository.

--- access.py
+++ access.py
@@ -15,7 +15,7 @@ def main():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
-
+    # Print the first 10 tweets that appear on my timeline
     for status in tweepy.Cursor(api.home_timeline).items(10):
         # Process a single status
         print(status.text)
