06fb10b05f1ee047e830c59029a34bda549b5bdd
Isaiah Chen <isaiahchen28@gmail.com>
2019-04-23 14:37:52 -0400

    Update pre_process.py

--- pre_process.py
+++ pre_process.py
@@ -1,8 +1,9 @@
 """
-This Python script is used to pre-process the data.
+This Python script contains functions that are used to pre-process the data.
 """
-import re
 import json
+import re
+
 
 emoticons_str = r"""
     (?:
@@ -18,13 +19,15 @@ regex_str = [
     r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
     # URLs
     r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',
+
     r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
     r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and '
     r'(?:[\w_]+)',  # other words
     r'(?:\S)'  # anything else
 ]
 
-tokens_re = re.compile(r'('+'|'.join(regex_str)+')', re.VERBOSE | re.IGNORECASE)
+tokens_re = re.compile(r'('+'|'.join(regex_str)+')',
+                       re.VERBOSE | re.IGNORECASE)
 emoticon_re = re.compile(r'^'+emoticons_str+'$', re.VERBOSE | re.IGNORECASE)
 
 
@@ -38,3 +41,9 @@ def preprocess(s, lowercase=False):
         tokens = [token if emoticon_re.search(
             token) else token.lower() for token in tokens]
     return tokens
+
+
+# with open("data/stream_youtube.json", 'r') as f:
+#     for line in f:
+#         tweet = json.loads(line)
+#         tokens = preprocess(tweet['text'])
