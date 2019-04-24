a6a4d0169c703e33777cfca8200ac137e751d8c8
Isaiah Chen <isaiahchen28@gmail.com>
2019-04-23 21:07:43 -0400

    Update collect_data.py
    The collect_data.py file should now have mostly completed docstrings and can handle time limits and timeout errors.

--- collect_data.py
+++ collect_data.py
@@ -3,20 +3,21 @@ This Python script is used to access Twitter and contains some basic functions
 for collecting data. To properly run this script, enter the following command
 in the terminal:
 
-python collect_data.py -q QUERY -d data
+python collect_data.py -q QUERY -d data -t time
 
-where QUERY is the query of interest and data is the directory where the json
-file will be saved to.
+where QUERY is the query of interest, data is the directory where the json
+file will be saved to, and time is the time that the stream will run for (in
+seconds). If the query does not appear in enough tweets after 5 seconds, the
+stream will time out and return an error.
 """
 import argparse
 import string
-import config
 import tweepy
-from tweepy import Stream
-from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
 import time
-import json
+import config
 
 
 def get_parser():
@@ -30,6 +31,8 @@ def get_parser():
     # Add argument for the directory for the file to be saved
     parser.add_argument("-d", "--data-dir", dest="data_dir",
                         help="The directory where the data will be saved in a .json file")
+    parser.add_argument("-t", "--time-limit", dest="time_limit",
+                        help="The desired amount of time to run the stream.")
     return parser
 
 
@@ -59,39 +62,71 @@ def format_filename(filename):
 class MyListener(StreamListener):
     """
     StreamListener object that is used to stream data from Twitter.
+
+    **Parameters**
+
+        data_dir: *str*
+            The name of the directory where the data will be stored.
+        query: *str*
+            The keyword of interest.
+        time_limit: *int*
+            The time limit for the stream to run
     """
 
-    def __init__(self, data_dir, query):
-        query_fname = format_filename(query)
-        self.outfile = "%s/stream_%s.json" % (data_dir, query_fname)
+    def __init__(self, data_dir, query, time_limit):
+        # Make sure the filename is formatted properly
+        query_filename = format_filename(query)
+        self.outfile = "%s/stream_%s.json" % (data_dir, query_filename)
+        self.start_time = time.time()
+        self.time_limit = time_limit
 
     def on_data(self, data):
-        try:
-            with open(self.outfile, 'a') as f:
-                f.write(data)
-                print(data)
-                return True
-        except BaseException as e:
-            print("Error on_data: %s" % str(e))
-            time.sleep(5)
-        return True
+        """
+        Write an individual Tweet to the output file. The data input is
+        encoded as a unicode type.
+        """
+        # Check if the time limit has been reached
+        if (time.time() - self.start_time) < self.time_limit:
+            try:
+                with open(self.outfile, 'a') as f:
+                    # Write data to the output file and print it to the
+                    # terminal
+                    f.write(data)
+                    print(data)
+                    return True
+            # Error handling
+            except BaseException as e:
+                print("Error on_data: %s" % str(e))
+                time.sleep(5)
+            return True
+        else:
+            print("Time limit has been reached.")
+            return False
 
     def on_error(self, status):
+        """
+        Print the error status in case an error occurs.
+        """
         print(status)
         return True
 
 
 def main():
+    # Define parser and retrieve the input arguments
     parser = get_parser()
     args = parser.parse_args()
+    # Retrieve API keys and tokens
     consumer_key = config.consumer_key
     consumer_secret = config.consumer_secret
     access_token = config.access_token
     access_secret = config.access_secret
+    # Authenticate user
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_secret)
     api = tweepy.API(auth)
-    twitter_stream = Stream(auth, MyListener(args.data_dir, args.query))
+    # Stream Twitter data using StreamListener object
+    twitter_stream = Stream(auth, MyListener(
+        args.data_dir, args.query, float(args.time_limit)), timeout=5)
     twitter_stream.filter(track=[args.query])
 
 
